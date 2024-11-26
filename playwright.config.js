d15926e1fa7b3d4788d0aa1418e3b3002e6c1294
Renan Santos <renan.santos@Renans-MBP.Home>
2024-11-26 22:53:30 +0000

    configure manual trigger for Playwright workflow and update environment secretsadd .env support for managing environment variables

--- playwright.config.js
+++ playwright.config.js
@@ -10,7 +10,8 @@ module.exports = defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['html', { outputFolder: 'output/playwright-report' }]],
+  outputDir: 'output/test-results',
   use: {
     baseURL: process.env.BASE_URL,
     screenshot: 'only-on-failure',
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -15,13 +15,12 @@ test.describe('Swag Labs Login Tests', () => {
     const productsHeader = page.locator('.title');
     await expect(productsHeader).toBeVisible();
     const headerText = await productsHeader.textContent();
-    console.log("Header Text: " + headerText);
 
     expect(headerText).toContain('Products');
   });
 
   test('Should show error message for locked out user', async ({ page }) => {
-    await page.getByPlaceholder('Username').fill(process.env.PASSWORD_STANDARD);
+    await page.getByPlaceholder('Username').fill(process.env.USERNAME_LOCKED);
     await page.getByPlaceholder('Password').fill(process.env.PASSWORD_LOCKED);
     await page.locator('input[data-test=login-button]').click();
 
@@ -29,7 +28,6 @@ test.describe('Swag Labs Login Tests', () => {
     await expect(errorLocator).toBeVisible();
 
     const messageError = await errorLocator.textContent();
-    console.log("Message Error: " + messageError);
 
     expect(messageError).toContain('Epic sadface: Sorry, this user has been locked out.');
   });
